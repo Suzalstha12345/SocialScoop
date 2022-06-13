fcfc6dcb2cb393a9d26fbdd7292dbaa9690611c2
Suzalstha12345 <shresthasujal32@gmail.com>
2022-06-13 11:38:16 +0545

    Testing

--- backend/database/connection.js
+++ backend/database/connection.js
@@ -1,7 +1,7 @@
-// const mongoose = require('mongoose');
+const mongoose = require('mongoose');
 
-// mongoose.connect('mongodb://127.0.0.1:27017/funbook', {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-// });
+mongoose.connect('mongodb://127.0.0.1:27017/funbook', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
 
